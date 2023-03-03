3a96b381b48627ec31c265642de4938f111c8604
Ash1eM <ashley.morris101@yahoo.com>
2023-03-03 18:43:39 -0500

    mod

--- proj.js
+++ proj.js
@@ -1,2 +1,23 @@
+// Prompt user # of rounds w integer
 let yourChoice
-yourChoice =prompt("Number of rounds")
+yourPrompt =prompt("Number of rounds")
+randNum = Math.round(Math.random()*3)
+// Loop through rounds and prompt
+let integer = parseInt(yourPrompt, 10);
+yourChoice = prompt("r,p, or s")
+// Conditional statements
+if (randNum=== 0){
+    compChoice = "r"
+} else if(randNum=== 1){
+    compChoice = "p"
+} else (randNum=== 2);{
+    compChoice = "s"
+}
+if (yourChoice=== compChoice){
+    alert("Winner")
+} else if (yourChoice> compChoice) {
+    alert("loser")
+} else (yourChoice < compChoice); {
+    alert("tie")
+}
+console.log(integer + yourChoice + compChoice)
