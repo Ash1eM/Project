7dcfebb27d5d2c67f0600fc9b85ef49499441ca7
Ash1eM <ashley.morris101@yahoo.com>
2023-04-03 13:06:35 -0400

    new

--- proj.js
+++ proj.js
@@ -1,34 +1,36 @@
 let numPrompt 
 numPrompt=parseInt(prompt("Number of rounds"))
+for(let i = 0; i < numPrompt; i++) {
 let yourChoice;
-yourChoice = prompt("Chose r,p, or s")
+yourChoice = prompt("Choose r,p, or s")
 // Conditional statements
 let compChoice
 compChoice = Math.round(Math.random()*3)
-for(let i = 0; i < 100; i++) {
 if (compChoice === 0){
     compChoice = "r"
 } else if(compChoice === 1){
     compChoice = "p"
-} else (compChoice === 2);{
+} else if (compChoice === 2) {
     compChoice = "s"
 }
 if (yourChoice=== compChoice){
     alert("It's a tie!")
 }  if (yourChoice==="p") {
     if (compChoice==="s")
-    alert("Loser");
+    alert("Loser")
 } else {
     alert("Winner!");
 } if (yourChoice=== "r") {
     if (compChoice==="p")
-    alert("Loser");
-} else {
-    alert("Winner!")
-}if (yourChoice=== "s") {
+    alert("Loser")
+}else {
+    alert("Winner")
+}
+if (yourChoice=== "s") {
     if (compChoice==="r")
-    alert("Loser");
+    alert("Loser")
+} else {
+    alert("Winner")
 }
+console.log(numPrompt);
 }
-
-
