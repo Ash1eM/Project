641b098b8647839791c0f148aa7f2d27648fe904
Ash1eM <ashley.morris101@yahoo.com>
2023-04-04 13:58:48 -0400

    changed

--- proj.js
+++ proj.js
@@ -3,6 +3,12 @@ for(let i = 0; i < numPrompt; i++) {
 let yourChoice;
 yourChoice = prompt("Choose r,p, or s")
 // Conditional statements
+let tie= 0
+let won = 0
+let loss = 0
+//tie++;
+//won++;
+//loss++;
 let compChoice
 compChoice = Math.round(Math.random()*3)
 if (compChoice === 0){
@@ -13,22 +19,16 @@ if (compChoice === 0){
     compChoice = "s"
 }
 if (yourChoice=== compChoice){
-    let tie=0
-    tie++;
-    alert("It's a tie!")
+    alert("It's a tie!");
 }  else if (yourChoice==="s" && compChoice==="p", 
  yourChoice==="p" && compChoice==="r",
-compChoice==="r" && compChoice==="s") {
-    let won =0
-    won++;
-    alert("You won!")
-} else (yourChoice==="s" && compChoice==="p",
- yourChoice==="p" && compChoice==="r",
-  compChoice==="r" && compChoice==="s");{
+yourChoice==="r" && compChoice==="s") {
+    alert("You won!");
+} else (compChoice==="s" && yourChoice==="p",
+ compChoice==="p" && yourChoice==="r",
+  compChoice==="r" && yourChoice==="s");{
     alert("You lost!");
-    let loss= 0
-    loss++;
-}
-}
+} 
+ } 
+alert("You won " +  + " rounds "+ "," + " You lost " +  +" rounds" +"," + " You tied " + + " rounds")
 console.log(numPrompt);
-alert("You won "+ won + " rounds " + "," + " You lost " + loss +" rounds" +"," + " You tied " + tie + " rounds")
